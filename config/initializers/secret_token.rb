3902de4c793af7a781b55881a26f9cb6aeb8bdc3
Feather <lisafeather@gmail.com>
2013-08-16 10:05:33 -0400

    installed backbone gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Raffler::Application.config.secret_key_base = '079101684192e8b57dc38d7e2972d5b1c274c5868a828fe548bd270f62a05a49ed0072685333f0f823e3f7a2897594ef6f23c60bec8a5a431429b3e73170609b'
